ae2f2b82ec3df2972c6dd2d80859815e1df0222d
Erfan Abdi <erfangplus@gmail.com>
2020-07-24 20:40:32 +0430

    ginkgo: Prepare to use in-tree display hal
    Change-Id: I4633eca7429a537b318f0e02d8c6871140a6180b
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Soong namespaces
+QCOM_SOONG_NAMESPACE := $(LOCAL_PATH)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
