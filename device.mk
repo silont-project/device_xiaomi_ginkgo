06656c4de69d0574c7e04c62290f63de6d1b6480
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-11 20:26:31 +0530

    sdm660-common: Build signapk jar
    Test: build
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Change-Id: Iab633fc9d450ec1354308a87f300550df73553bb

--- device.mk
+++ device.mk
@@ -413,6 +413,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
