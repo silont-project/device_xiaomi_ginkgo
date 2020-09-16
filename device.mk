8159f40a7fa98383fda73b3dd9f6a7c286524c3d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-16 19:42:08 +0530

    ginkgo: Nuke secure_element HAL service
     * doesn't work and crashes on willow

--- device.mk
+++ device.mk
@@ -65,7 +65,6 @@ PRODUCT_COPY_FILES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
-    android.hardware.secure_element@1.0-service \
     NfcNci \
     Tag \
     SecureElement
